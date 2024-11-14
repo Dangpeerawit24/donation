6d58a448234575f04edf7f30527425229f3486e8
Peerawit Chanta <158536939+Dangpeerawit24@users.noreply.github.com>
2024-11-14 11:26:32 +0700

    update

--- resources/views/pushevidence.blade.php
+++ resources/views/pushevidence.blade.php
@@ -8,8 +8,9 @@
     <link rel="icon" type="" href="{{ asset('img/AdminLogo.png') }}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
 </head>
 
 <body>
@@ -45,9 +46,7 @@
                 </div>
                 <input type="hidden" name="userid" value="{{ $lineId }}">
                 <input type="hidden" id="transactionID" name="transactionID" value="{{ $transactionID }}">
-                @foreach ($names as $name)
-                    <input type="hidden" id="campaignsname" name="campaignsname" value="{{ $name->campaignsname }}">
-                @endforeach
+                <input type="hidden" id="transactionID" name="transactionID" value="{{ $campaignsname }}">
                 <div class="d-flex justify-content-center align-items-center mt-3">
                     <button class="btn btn-success" type="button" onclick="submitForm()">ยืนยันหลักฐาน</button>
                 </div>
@@ -77,11 +76,6 @@
                     closeOnEsc: false
                 });
 
-                // เพิ่มดีเลย์เล็กน้อยก่อนส่งฟอร์ม
-                setTimeout(() => {
-                    document.getElementById("uploadForm").submit();
-                }, 1000); // ดีเลย์ 1 วินาที
-
                 document.getElementById("uploadForm").submit();
             }
         }
@@ -94,8 +88,8 @@
                 timer: 5000,
                 showConfirmButton: false
             });
-            @endif
         </script>
+    @endif
 </body>
 
 </html>
