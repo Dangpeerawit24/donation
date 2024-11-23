ace2cb899a7071e73aa7a6b4ef64b60f5dc95f58
Peerawit Chanta <158536939+Dangpeerawit24@users.noreply.github.com>
2024-11-23 21:21:44 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     </div>
     <div class=" grid row w-full h-full my-20 content-center justify-center">
         @if ($campaigns->isEmpty())
-            <div class="text-center w-full h-screen items-center text-gray-500 mt-10">
+            <div class="text-center w-full  items-center text-gray-500 mt-10">
                 <p class="text-xl">ขออภัย ไม่มีกองบุญที่เปิดให้ร่วมบุญในขณะนี้</p>
             </div>
         @endif
